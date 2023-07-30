6c596796ea5e5729e1f057c88aff00595bb91558
Dounya Derlen <dounyaderlen@MacBook-Air-de-Dounya.local>
2023-07-30 13:23:24 +0200

    add - loader animation

--- src/components/Loader.vue
+++ src/components/Loader.vue
@@ -5,19 +5,34 @@
   </div>
 </template>
 <style scoped>
-html, body, div {
+/* Animation du logo */
+@keyframes logoAnimation {
+  0%,
+  100% {
+    opacity: 1;
+    transform: scale(1);
+  }
+  50% {
+    opacity: 0.1;
+    transform: scale(1.1);
+  }
+}
+html,
+body,
+div {
   cursor: wait;
   width: 100%;
   height: 100%;
   background: white;
   padding: 0;
   margin: 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  min-height:80vh
 }
 #logoDD {
-  position: absolute;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
   max-width: 300px;
+  animation: logoAnimation 2s infinite;
 }
 </style>
