9f3885fd2b4f923d8d5c0bc17b2e381149e828b4
Dounya Derlen <dounyaderlen@MacBook-Air-de-Dounya.local>
2023-09-25 14:50:10 +0200

    add - new project translation

--- src/plugins/i18n.js
+++ src/plugins/i18n.js
@@ -21,7 +21,8 @@ const i18n = createI18n({
         'rocketfeed': 'Ce projet est un effort de groupe et aussi une de mes plus grandes fiertés. C\'est un agrégateur de news sans redirection vers la source de l\'information. Tout y est paramétrable, l\'utilisateur peut contrôler le type d\'information qu\'il voit ainsi que la source. Le plus intéréssant a été l\'intégration des réseaux sociaux dans le feed comme Youtube ou Twitter. Sur ce projet je suis l\'auteur de la charte graphique ainsi que de l\'UX entière. Je l\'ai présenté avec un groupe de 5 développeurs en tant que projet de fin d\'année et ensemble nous avons obtenu la note maximale.',
         'glass': 'J\'ai créé cette landing page en suivant un tutoriel sur Youtube sur le glassmorphisme. C\'est un style que j\'affectionne particulièrement car beaucoup utilisé notamment chez Apple par sa sobriété et son aspect passe partout',
         'coco': 'Immensément créative et jamais à court d\'idées',
-        'fullstack': 'Développeur web full-stack'
+        'fullstack': 'Développeur web full-stack',
+        'food': "Ces prototypes ont été réalisés en s'inspirant d'un tutoriel Adobe XD, en utilisant seulement Figma"
       }
     },
     en: {
@@ -42,7 +43,8 @@ const i18n = createI18n({
         'rocketfeed': 'This project is a group effort and also one of my greatest prides. It is a news aggregator who is not redirecting to the source of the information. Everything is configurable, the user can control the type of information he sees as well as the source. The most interesting part was the integration of social networks in the stream like Youtube or Twitter. On this project, I am the author of the graphic charter as well as the entire UX. I presented it with a group of 5 developers as an end-of-year project and together we got the maximum grade.',
         'glass': 'I created this landing page by following a Youtube tutorial on glassmorphism. It is a style that I particularly fancy because it is used a lot (especially Apple products), due to its sobriety and its ordinary-looking appearance',
         'coco': 'Immensely creative and never runs out of ideas',
-        'fullstack': 'Full stack web developer'
+        'fullstack': 'Full stack web developer',
+        'food': "These prototypes were made based on an Adobe XD tutorial, but only using Figma."
       }
     }
   }
